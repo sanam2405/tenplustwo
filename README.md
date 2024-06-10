6925b89e776d7c26cd88637c09d6fe96a52a756b
Manas Pratim Biwas <manaspb2405@gmail.com>
2024-06-10 09:46:18 +0530

    [feat]: MVP

--- README.md
+++ README.md
@@ -1,4 +1,38 @@
 # tenplustwo
 
-_CLI to fetch and save HS and Madhyamik results in a bulk_ 
+_CLI to fetch and save HS and Madhyamik results in a bulk_
 
+## Setting up locally
+
+- Clone the tenplustwo repository
+
+```bash
+    git clone git@github.com:sanam2405/tenplustwo.git
+    cd rp
+```
+
+- Install the dependencies
+
+```bash
+    npm install
+```
+
+- Build the source
+
+```bash
+    npm run build
+```
+
+- Fetch the results
+
+```bash
+   npm start -- -y <year> -r <roll> -l <lower_limit> -u <upper_limit>
+   npm start -- --year <year> --roll <roll> --lower <lower_limit> --upper <upper_limit>
+```
+
+Example
+
+```bash
+   npm start -- -y 2024 -r 432521 -l 1247 -u 1350
+   npm start -- --year 2024 --roll 432521 --lower 1247 --upper 1350
+```
---
+++ src/index.ts
@@ -0,0 +1,87 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import { program } from 'commander';
+
+dotenv.config();
+
+interface Options {
+  year: string;
+  roll: string;
+  lower: string;
+  upper: string;
+}
+
+program
+  .option('-y, --year <year>', 'Year of examination')
+  .option('-r, --roll <roll>', 'Roll number')
+  .option('-l, --lower <lower>', 'Lower limit of rno')
+  .option('-u, --upper <upper>', 'Upper limit of rno')
+  .parse(process.argv);
+
+const options: Options = program.opts() as Options;
+// console.log("Parsed options:", options);  
+
+Promise.resolve(options).then(({ year, roll, lower, upper }) => {
+  postResult(parseInt(year), roll, parseInt(lower), parseInt(upper));
+});
+
+async function postResult(year: number, roll: string, lower: number, upper: number) {
+  const { default: fetch } = await import('node-fetch');
+  const BASE_URL = process.env.URL;
+
+  if (!BASE_URL) {
+    console.error('Base URL is not defined in the environment variables.');
+    return;
+  }
+
+  if (!year || !roll || !lower || !upper) {
+    console.log('All the flags are not provided');
+    return;
+  }
+
+  // Determine the current directory
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const resultDir = path.join(__dirname, '../results', `HS${year}`);
+
+  if (fs.existsSync(resultDir)) {
+    fs.rmSync(resultDir, { recursive: true });
+  }
+
+  fs.mkdirSync(resultDir, { recursive: true });
+
+  const url = `${BASE_URL}/highersecondary${year}/wbhsresult${year % 100}.asp`;
+
+  const headers = {
+    'Referer': BASE_URL,
+    'Content-Type': 'application/x-www-form-urlencoded'
+  };
+
+  for (let rno = lower; rno <= upper; rno++) {
+    const payload = new URLSearchParams({
+      roll: roll,
+      rno: String(rno)
+    });
+
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: headers,
+        body: payload.toString()
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const result = await response.text();
+      const filename = path.join(resultDir, `${roll}-${rno}.html`);
+      fs.writeFileSync(filename, result);
+      console.log(`Result for Roll ${roll} Number ${rno} saved to ${filename}`);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+}
